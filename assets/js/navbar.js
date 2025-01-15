7210bdc7b4d0d99253ec030257a606b153bac888
yasasvi <yasasvigumma@gmail.com>
2025-01-15 09:48:57 +0530

    Done with changes for now.
    good enough!

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -4,3 +4,17 @@ document.addEventListener("scroll", () => {
     ? l.classList.add("scrolled")
     : l.classList.remove("scrolled");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const gearIcon = document.querySelector(".gear");
+  const sidebar = document.querySelector(".sidebar");
+  const main = document.querySelector("main");
+  
+  gearIcon.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+  });
+
+  main.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+  })
+});
